543d650a7b313f05aa5d334f52134dc175dcab2d
kevlouis <114162535+kevlouis@users.noreply.github.com>
2025-05-13 21:14:44 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
+const root = document.getElementById("root");
+
+const title = document.createElement("h1");
+title.textContent = "VULTURE";
+
+const subtitle = document.createElement("p");
+subtitle.textContent = "Le réseau des stars de demain.";
+
+const message = document.createElement("p");
+message.style.marginTop = "2rem";
+message.textContent = "Ce site est en cours de développement. Revenez bientôt.";
+
+root.appendChild(title);
+root.appendChild(subtitle);
+root.appendChild(message);
 
